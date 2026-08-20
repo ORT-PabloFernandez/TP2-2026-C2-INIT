0dd84be120e340badef2ec8526a18a3f8fc4b92d
Pablo Hinojosa <pablo.hinojosa@ort.edu.ar>
2026-08-20 23:50:19 +0000

    arrays

--- basic/arrays.js
+++ basic/arrays.js
@@ -11,3 +11,25 @@ const inventors = [
   { first: "Max", last: "Planck", year: 1858 },
 ];
 
+function printInventors(inventors) {
+    console.log("NOMBRE     APELLIDO     AÑO");
+    console.log("============================");
+
+    // un callback es un funcion enviada por parametro a otra funcion
+    // forEach es un funcion que itera un array y llama al callback por cada elemento del array
+    inventors.forEach(
+        inventor => console.log(`${inventor.first.padEnd(12, " ")}${inventor.last.padEnd(12, " ")}${inventor.year}`)
+    );
+}
+
+// function printInventor(inventor) {
+//     console.log(`${inventor.first.padEnd(12, " ")}${inventor.last.padEnd(12, " ")}${inventor.year}`);
+// }
+
+//const printInventor = inventor => console.log(`${inventor.first.padEnd(12, " ")}${inventor.last.padEnd(12, " ")}${inventor.year}`);
+
+// Ejercicio 1.
+// Filtrar los inventores que nacieron en el siglo XIX (1800)
+printInventors(inventors.filter(inventor => inventor.year >= 1800));
+
+//printInventors(inventors);
